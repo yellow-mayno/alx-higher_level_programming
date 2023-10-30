fc0ddc9af0a0439440831ec741652939668b23e9
python <python@snake.com>
2023-10-30 23:23:39 +0630

    autocommit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+    int i, n = 1;
+    listint_t *ptrtest, *previous, *next;
+
+    if (list != NULL)
+    {
+        ptrtest = list->next;
+        while (ptrtest != NULL)
+        {
+            next = list;
+            i = 0;
+            while (next != NULL & i < n)
+            {
+                if (next == ptrtest)
+                    return (1);
+                next = next->next;
+                i++;
+            }
+            ptrtest = ptrtest->next;
+            n++;
+        }
+    }
+    return (0);
+}
