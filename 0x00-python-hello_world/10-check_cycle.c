6a393625039210e93db79af4b0a5d82b0ff8dab7
python <python@snake.com>
2023-10-30 23:57:21 +0630

    autocommit

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,27 +1,32 @@
 #include "lists.h"
 
+/**
+* check_cycle - check for cycle in single linked list
+* @list: head of the list
+* Return: 0 if none 1 otherwise
+*/
+
 int check_cycle(listint_t *list)
 {
 	int i, n = 1;
 	listint_t *ptrtest, *next;
 
-	if (list != NULL)
+	if (list == NULL)
+		return (0);
+	ptrtest = list->next;
+	while (ptrtest != NULL)
 	{
-		ptrtest = list->next;
-		while (ptrtest != NULL)
+		next = list;
+		i = 0;
+		while (next != NULL && i < n)
 		{
-			next = list;
-			i = 0;
-			while (next != NULL && i < n)
-			{
-				if (next == ptrtest)
-				return (1);
-				next = next->next;
-				i++;
-			}
-			ptrtest = ptrtest->next;
-			n++;
+		if (next == ptrtest)
+		return (1);
+		next = next->next;
+		i++;
 		}
+		ptrtest = ptrtest->next;
+		n++;
 	}
 	return (0);
 }
