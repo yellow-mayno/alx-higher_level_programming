12958805c97677de3041dee49da2a724168d0ea7
python <python@snake.com>
2023-11-07 00:26:02 +0630

    autocommit

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for line in matrix:
+        for i in range(len(line)):
+            print("{:d}".format(line[i]), end='')
+            if i != len(line) - 1:
+                print("{}".format(" "), end='')
+        print("")
