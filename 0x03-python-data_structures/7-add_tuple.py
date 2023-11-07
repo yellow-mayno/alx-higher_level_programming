158724590774fb4b358316ab875584df6c458223
python <python@snake.com>
2023-11-07 13:08:03 +0630

    autocommit

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    return (tuple_a[0] + tuple_b[0],tuple_a[1] + tuple_b[1])
+	l = []
+	for i in range(2):
+		try:
+			l.append(tuple_a[i])
+		except IndexError :
+			l.append(0)
+		try:
+			l.append(tuple_b[i])
+		except IndexError :
+			l.append(0)
+	return (l[0] + l[1], l[2] + l[3])
---
+++ 0x03-python-data_structures/multiple_returns.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def multiple_returns(str):
+	return len(str),str[0]
