87d6bfd9a21934fd79a33f7116fa9c41cec5b123
king_necromonger <riddick@furya.necro>
2023-11-20 18:34:07 -0600

    auto_commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for e in my_list:
             if i == x:
                 break
-            print(e, end=" ")
+            print("{}".format(e), end='\0')
             i = i + 1
         print()
     finally:
