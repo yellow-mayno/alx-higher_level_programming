bff677dda9c3d0b83205b34bc72c76abb599ee63
king_necromonger <riddick@furya.necro>
2023-11-21 21:27:58 +0000

    auto_commit

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """
+    empty
+    """
+    def __init__(self, size=0):
+        """
+        empty
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        empty
+        """
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """
+        empty
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """
+        empty
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def my_print(self):
+        """
+        empty
+        """
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
+
+    @position.setter
+    def position(self, value):
+        """
+        empty
+        """
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
