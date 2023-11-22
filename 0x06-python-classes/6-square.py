2ae534419d2e7dda6ee2312b56333b2e2e09aebc
king_necromonger <riddick@furya.necro>
2023-11-22 23:08:01 +0000

    auto_commit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -53,7 +53,7 @@ class Square:
         """empty
 
         Args:
-            size (int): lkjlkjlk
+            value (int): lkjlkjlk
         """
         if not isinstance(value, tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
