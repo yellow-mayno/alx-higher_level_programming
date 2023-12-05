0d987b35084cb7ede9c0a60dc275ef917871cd26
king_necromonger <riddick@furya.necro>
2023-12-05 21:50:57 +0000

    auto_commit

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" gfhd dfg fd dflkj fg lf """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ dfg dsfg sdfg sdfg sfgds
+
+    Args:
+        my_obj: object
+        filename: filename
+
+    Raises:
+        Exception: dfgsdf fdsg dsfg fdg
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" gfhd dfg fd dflkj fg lf """
+import json
+
+
+def save_to_json_file(my_obj):
+    """ dfg dsfg sdfg sdfg sfgds
+
+    Args:
+        my_obj: object
+
+    Raises:
+        Exception: dfgsdf fdsg dsfg fdg
+    """
+    with open(my_obj, 'r', encoding="utf-8") as f:
+        return json.load(f)
