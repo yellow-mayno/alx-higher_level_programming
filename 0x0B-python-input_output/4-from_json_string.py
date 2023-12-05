6cf1f6f8a2c62e50a8613791a6d8bcff8ec761e5
king_necromonger <riddick@furya.necro>
2023-12-05 21:30:58 +0000

    auto_commit

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" sdfsda dsf sdf sdf sdf sd """
+
+import json
+
+
+def from_json_string(my_obj):
+    """ sdfsdfsd sdf sd sdf sd df s
+
+    Args:
+        my_obj: object
+
+    Raises:
+        Exception: sdfsdfs df se ensdf sdfcodsd sdfsed
+
+    """
+    return json.loads(my_obj)
