a0c295a01818c7f66cd3db218379cc14c4c3ecf8
king_necromonger <riddick@furya.necro>
2023-12-05 21:25:30 +0000

    auto_commit

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" dfgsdf dsfg dsfgfgs sfdg"""
+
+
+def append_write(filename="", text=""):
+    """ fgfd gfdsgdsf gdsf
+
+    Args:
+        filename: filename
+        text: text
+
+    Raises
+        Exception: dsfgsd dfgsdfg sfdg
+
+    """
+
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
