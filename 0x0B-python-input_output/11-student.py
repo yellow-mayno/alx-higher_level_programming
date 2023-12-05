72989ea7b4b21fdfbefa80609703a7d0aec81f50
king_necromonger <riddick@furya.necro>
2023-12-05 23:06:30 +0000

    auto_commit

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" fghd fg hdfgh df """
+
+
+class Student:
+    """ hfg fgh fgh fgh
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ gfh gfh fgh fgh fg """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """  fgh fgh fgh fg fgh """
+        if type(attrs) == list and attrs is not None:
+            temp = {}
+            for elem in attrs:
+                if type(elem) != str:
+                    return self.__dict__
+                if elem in self.__dict__.keys():
+                    temp[elem] = self.__dict__[elem]
+            return temp
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """ dsf sad sdf sdf """
+        for elem in json:
+            self.__dict__[element] = json[element]
