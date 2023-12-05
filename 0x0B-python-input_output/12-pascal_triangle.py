8be222b8f1cd0baa0b4709d08baac89ad759bbcb
king_necromonger <riddick@furya.necro>
2023-12-05 23:46:19 +0000

    auto_commit

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" dsfsd fsd
+"""
+
+
+def pascal_triangle(n):
+    """ fdsgsfdg sdf gdsf gsfd g
+
+    Args:
+        filename: filename
+        text: text
+
+    """
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [1]
+    elif n == 2:
+        return [[1], [1, 1]]
+    else:
+        current = []
+        new = pascal_triangle(n - 1).copy()
+        previous = new[-1]
+        current.append(1)
+        i = 0
+        for i in range(len(previous) - 1):
+            current.append(previous[i] + previous[i + 1])
+        current.append(1)
+        new.append(current)
+        return new
