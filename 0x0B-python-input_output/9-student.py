351d87f8f8b051f543a4adaa6d1dc92cbe4e8cea
king_necromonger <riddick@furya.necro>
2023-12-05 22:47:18 +0000

    auto_commit

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" fghd fg hdfgh df """
+
+
+class Student:
+    """ hfg fgh fgh fgh  """
+
+
+    def __init__(self, first_name, last_name, age):
+        """ gfh gfh fgh fgh fg """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """  fgh fgh fgh fg fgh """
+        return self.__dict__.copy()
