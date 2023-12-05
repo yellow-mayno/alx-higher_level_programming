74a207773eea1f0b392e5b7a687f33a3e25b5d9a
king_necromonger <riddick@furya.necro>
2023-12-05 21:19:16 +0000

    auto_commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" tfdgsdfg sdfg sdfg """
+
+
+def read_file(filename=""):
+    """ dfdsg df gdsfg dsfg 
+
+    Args:
+        filename: filename
+
+    Raises
+        Exception: sdfgdsfg fdgds
+
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
