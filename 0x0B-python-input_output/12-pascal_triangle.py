ac90fe44d988c7e55f297f210125b4bcbb4e63cf
king_necromonger <riddick@furya.necro>
2023-12-06 00:16:12 +0000

    auto_commit

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -10,24 +10,21 @@ def pascal_triangle(n):
 
     """
 
-    try:
-        if n <= 0:
-            return []
-        elif n == 1:
-            return [1]
-        elif n == 2:
-            return [[1], [1, 1]]
-        else:
-            current = []
-            new = pascal_triangle(n - 1).copy()
-            previous = new[-1]
-            current.append(1)
-            i = 0
-            for i in range(len(previous) - 1):
-                current.append(previous[i] + previous[i + 1])
-            current.append(1)
-            new.append(current)
-            return new
-    except TypeError:
-        print("{}".format("type error"))
+    if n <= 0:
         return []
+    elif n == 1:
+        return [1]
+    elif n == 2:
+        return [[1], [1, 1]]
+    else:
+        current = []
+        new = pascal_triangle(n - 1).copy()
+        previous = new[-1]
+        current.append(1)
+        i = 0
+        for i in range(len(previous) - 1):
+            current.append(previous[i] + previous[i + 1])
+        current.append(1)
+        new.append(current)
+        return new
+    
