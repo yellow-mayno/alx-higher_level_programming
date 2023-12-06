4e68585194db5d73a0f92257ff8b56392fe4373a
king_necromonger <riddick@furya.necro>
2023-12-06 00:14:45 +0000

    auto_commit

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-""" dsfsd fsd
-"""
-
-
 def pascal_triangle(n):
     """ fdsgsfdg sdf gdsf gsfd g
 
@@ -10,23 +6,28 @@ def pascal_triangle(n):
         n: filename
 
     Returns:
-        list: HASDKJF SLKJD JHFDSHF LKJ
+        list: dfsgdf dfg dfs dfg
 
     """
-    if n <= 0:
+
+    try:
+        if n <= 0:
+            return []
+        elif n == 1:
+            return [1]
+        elif n == 2:
+            return [[1], [1, 1]]
+        else:
+            current = []
+            new = pascal_triangle(n - 1).copy()
+            previous = new[-1]
+            current.append(1)
+            i = 0
+            for i in range(len(previous) - 1):
+                current.append(previous[i] + previous[i + 1])
+            current.append(1)
+            new.append(current)
+            return new
+    except TypeError:
+        print("{}".format("type error"))
         return []
-    elif n == 1:
-        return [1]
-    elif n == 2:
-        return [[1], [1, 1]]
-    else:
-        current = []
-        new = pascal_triangle(n - 1).copy()
-        previous = new[-1]
-        current.append(1)
-        i = 0
-        for i in range(len(previous) - 1):
-            current.append(previous[i] + previous[i + 1])
-        current.append(1)
-        new.append(current)
-        return new
