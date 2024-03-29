2c29bacb6008134d35eaae4b3bd7617d83c2def0
yellow-mayno <chadi_boulahya@outlook.fr>
2024-03-30 00:36:44 +0100

    auto

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,2 @@
 -- max temp per state
-SELECT state, MAX(value) AS max_temp FROM temperatures GROUP B
-Y state ORDER BY state;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
