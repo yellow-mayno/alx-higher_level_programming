f7a8b95caa5f40bb04b55398a8ab32d836ee55bd
yellow-mayno <chadi_boulahya@outlook.fr>
2024-04-04 18:48:40 +0200

    auto

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- creating a user that only SELECT
-CREATE IF NOT EXISTS DATABASE hbtn_0d_2;
-CREATE IF NOT EXISTS USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-ALTER USER 'user_0d_2'@'localhost' IDENTIFIED BY 'uesr_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+ALTER USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
-GRANT ONLY SELECT ON 'htbn_0d_2' 
+GRANT ONLY SELECT ON 'htbn_0d_2'.* TO 'user_0d_2'@'localhost'; 
