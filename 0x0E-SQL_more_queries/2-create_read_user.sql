85067a363096e5663259812cd2423ee7fa4bb0d6
yellow-mayno <chadi_boulahya@outlook.fr>
2024-04-04 19:09:18 +0200

    auto

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 ALTER USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
-GRANT ONLY SELECT ON 'htbn_0d_2'.* TO 'user_0d_2'@'localhost'; 
+GRANT SELECT ON htbn_0d_2.* TO 'user_0d_2'@'localhost'; 
