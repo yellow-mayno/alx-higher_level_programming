8bbb095b8c3c7a9599e482c54f9fbd56a228505d
yellow-mayno <chadi_boulahya@outlook.fr>
2024-04-06 23:01:08 +0200

    auto

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,5 @@
 -- create a table
 -- create table will overwrite the previous
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
+use hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(id INT PRIMARY KEY AUTO_INCREMENT UNIQUE NOT NULL, state_id INT NOT NULL, name VARCHAR(256) NOT NULL);
