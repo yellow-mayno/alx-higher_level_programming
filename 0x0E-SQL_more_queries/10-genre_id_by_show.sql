7c6afa9118a3d6b4b64350b0ab1063126a0b3276
yellow-mayno <chadi_boulahya@outlook.fr>
2024-04-11 15:56:29 +0200

    auto

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- cross the table of shows and the table of genre
 -- for each show lists the genres that the show belongs to
-SELECT tv_shows.title AS ttle  , tv_show_genres.genre_id AS gnre FROM tv_show_genres INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id ORDER BY title, genre;
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id FROM tv_show_genres INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id ORDER BY title, genre_id;
