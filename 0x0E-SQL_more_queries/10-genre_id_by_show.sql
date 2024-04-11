d5fb8c05a12b27d3d597de714b7cb3b0a1525af2
yellow-mayno <chadi_boulahya@outlook.fr>
2024-04-11 20:23:34 +0200

    auto

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,4 @@
 -- cross the table of shows and the table of genre
 -- for each show lists the genres that the show belongs to
+-- edit: should have done that with an INNER JOIN
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id FROM tv_show_genres INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id ORDER BY title, genre_id;
---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,3 @@
+-- cross the table of shows and the table of genre
+-- for each show lists the genres that the show belongs to
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id FROM tv_show_genres RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id ORDER BY title, genre_id;
