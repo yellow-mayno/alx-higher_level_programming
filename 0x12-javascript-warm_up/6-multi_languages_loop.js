fa3027b7b4d81606dde6a021ad513581af1f4b7d
yellow-mayno <chadi_boulahya@outlook.fr>
2024-04-28 22:10:45 +0200

    auto

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const varC = 'C is fun';
+const varP = 'Python is cool';
+const varJS = 'Javascript is amazing';
+const list = [varC, varP, varJS];
+list.forEach((val, ind) => {
+  console.log(val);
+});
