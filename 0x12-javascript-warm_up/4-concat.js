5e0ba310c2a14c74017e64ec20bf5d3e2a83e6be
yellow-mayno <chadi_boulahya@outlook.fr>
2024-04-28 19:18:27 +0200

    auto

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = [];
+const { argv } = require('node:process');
+argv.forEach((ind, val) => {
+	args.push(ind);
+});
+args.push('undefined');
+args.push('undefined');
+console.log(`${args[2]} is ${args[3]}`);
