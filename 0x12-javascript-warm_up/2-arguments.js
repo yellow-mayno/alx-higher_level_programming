7da1db634aeed5cc1362d604264e90d363fd4965
yellow-mayno <chadi_boulahya@outlook.fr>
2024-04-28 11:37:55 +0200

    auto

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
-:
+const { argv } = require('node:process');
+console.log(argv.length);
+if (argv.length == 2) {
+	console.log('No argument');
+} else {
+	console.log('Argument found');
+}
