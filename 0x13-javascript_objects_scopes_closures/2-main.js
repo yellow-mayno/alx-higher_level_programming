23d4c77918efbfabd7d3f8d0983a06ac70e598d6
yellow-mayno <chadi_boulahya@outlook.fr>
2024-04-29 17:54:42 +0200

    auto

---
+++ 0x13-javascript_objects_scopes_closures/2-main.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const Rectangle = require('./2-rectangle');
+const r1 = new Rectangle();
+console.log(r1);
+console.log(r1.constructor);
---
+++ 0x13-javascript_objects_scopes_closures/test.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+let myvar = 'q';
+console.log(isNaN(myvar));
