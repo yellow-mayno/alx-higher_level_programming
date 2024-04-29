d0a81345a96ecaf66ecfcc5693e50978c4c7442c
yellow-mayno <chadi_boulahya@outlook.fr>
2024-04-29 12:34:26 +0200

    auto

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,4 +1,15 @@
 #!/usr/bin/node
-exports.incr = function () {
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
   this.value = this.value + 1;
 };
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
