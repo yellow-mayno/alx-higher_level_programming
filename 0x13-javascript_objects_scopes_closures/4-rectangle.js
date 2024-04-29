cab4d869467ab4d2e2a8a0480416a8413ed96387
yellow-mayno <chadi_boulahya@outlook.fr>
2024-04-30 00:45:23 +0200

    auto

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (isNaN(w) || isNaN(h)) {
+      return;
+    } else if (w <= 0 || h <= 0) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    let i = 0;
+    for (i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+
+}
+
+module.exports = Rectangle;
